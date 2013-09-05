d99333ad3c619bca419c2bc1340daebab6b0749d
Kaarel Kaljurand <kaljurand@gmail.com>
2013-09-05 08:34:43 +0200

    Add script for noun coverage testing

---
+++ tools/test-noun-coverage.sh
@@ -0,0 +1,23 @@
+#/bin/sh
+
+gold=nouns.6forms.csv
+out=nouns.6forms.gf.csv
+diff=nouns.6forms.diff.csv
+coverage=coverage.txt
+
+g=../estonian/
+cat ${gold} |\
+sed "s/,.*//" |\
+python cc.py -r ${g}/HjkEst.gf --oper "hjk_type" |\
+gf --run |\
+perl -nal -F",\s+" -e 'print "$F[0], $F[1], $F[2], $F[3], $F[15], $F[16]"' > ${out}
+
+./diff-list-set.py ${gold} ${out} | tee ${diff} | cut -f2 | sort | uniq -c | sort -nr | tee ${coverage}
+
+total=`cat ${out} | wc -l`
+correct=`head -1 ${coverage} | sed "s/ *//g"`
+result=`echo "scale=3; ${correct}/${total}" | bc`
+
+echo "Coverage: ${correct} out of ${total} = ${result}"
+
+diff coverage.prev ${coverage}
