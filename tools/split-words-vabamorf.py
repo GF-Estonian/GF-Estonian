5b91659bb4d26fb6f13f86b50c6eb393babd9512
Kaarel Kaljurand <kaljurand@gmail.com>
2014-06-22 19:17:36 +0200

    Generalize split-nouns -> split-words(-vabamorf)

--- tools/split-words-vabamorf.py
+++ tools/split-words-vabamorf.py
@@ -3,19 +3,36 @@
 # Splits noun compounds using Vabamorf.
 # Requires pyvabamorf (https://github.com/brainscauseminds/pyvabamorf)
 
+# Adjective POS tags are: A, D, U.
+# We consider only A (the positive form).
+
 import sys
 import re
+import argparse
 from pyvabamorf import PyVabamorf
 from pprint import pprint
 
-def is_noun_lemma(x):
-    return x['partofspeech'] == 'S' and x['form'] == 'sg n'
 
-def select_splits(a):
-    return [ x['lemma_tokens'] for x in filter(is_noun_lemma, a[0]['analysis']) ]
+def is_lemma_form(pos, x):
+    return x['partofspeech'] == pos and x['form'] == 'sg n'
+
+funs_filter = {
+    'S': lambda x : is_lemma_form('S', x),
+    'A': lambda x : is_lemma_form('A', x)
+}
+
+def select_splits(f, a):
+    return [ x['lemma_tokens'] for x in filter(f, a[0]['analysis']) ]
 
 def remove_symbols(a):
     return re.sub(r'[]?=+]', '', a).encode('utf8')
+
+p = argparse.ArgumentParser(description='')
+p.add_argument('--pos', type=str, action='store', dest='pos', default='S', help='part of speech, one of ' + str(funs_filter.keys()))
+p.add_argument('-v', '--version', action='version', version='%(prog)s v0.1')
+args = p.parse_args()
+
+fun_filter = funs_filter[args.pos]
 
 m = PyVabamorf()
 
@@ -23,7 +40,7 @@ for line in sys.stdin:
     line = line.strip()
     try:
         result = m.analyze([line])
-        splits = select_splits(result)
+        splits = select_splits(fun_filter, result)
         # We take just the first solution
         parts = [remove_symbols(x) for x in splits[0]]
         if ''.join(parts) == line:
