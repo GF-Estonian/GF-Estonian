deca9fe7478b41085288035d4dce18723ad94772
Kaarel Kaljurand <kaljurand@gmail.com>
2015-08-05 21:45:52 +0200

    Minor fixes in DictionaryEst.gf

--- tests/run-wide.sh
+++ tests/run-wide.sh
@@ -10,7 +10,7 @@ tests_gold="../../test-suite/translate_eng_est/tests.gold.tsv"
 pgf="../Translate2.pgf"
 
 # Run this only if the English parser has changed
-#time cat ${tests_in} | translate.py --pgf $pgf --tokenize --source TranslateEng --target TranslateEst > ${tests_out}
+#time cat ${tests_in} | translate.py --pgf $pgf --tokenize=nltk --source TranslateEng --target TranslateEst > ${tests_out}
 
 # Otherwise run only the linearization
 time cat ${tests_gold} | translate.py --pgf $pgf --source TranslateEng --target TranslateEst --type=gold > ${tests_out}
